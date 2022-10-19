90884c8c07ee88b386b720bd6bd1d4615781b6c7
Aaditya Muley <aadityamuley8190@gmail.com>
2022-10-19 14:22:17 -0700

    Buyer and seller authentication complete.

--- Facade.java
+++ Facade.java
@@ -1,14 +1,26 @@
 import java.io.FileNotFoundException;
 
+
 public class Facade {
 
     int userType;
+    String credentials;
+    boolean authenticationStatus;
+    
     int productType;
 
-    public void login() throws FileNotFoundException {
+    public void login(int user, String username, String password) throws FileNotFoundException {
+
+        userType = user;
+        credentials = username + ":" + password;
+
         Login login = new Login();
-        boolean check = login.buyerLogin("tutu:1111");
-        check = login.sellerLogin("pepe:3333");
+        if(userType == 0) {
+            authenticationStatus = login.buyerLogin(credentials);
+        }
+        else {
+            authenticationStatus = login.sellerLogin(credentials);
+        }
     }
 
     
--- Main.java
+++ Main.java
@@ -1,11 +1,25 @@
 import java.io.FileNotFoundException;
 import java.nio.file.FileAlreadyExistsException;
+import java.util.Scanner;
+
 
 public class Main {
+
+    static Scanner sc = new Scanner(System.in);
     
     Facade facade = new Facade();
     public static void main(String[] args) throws FileNotFoundException{
+
+        System.out.println("Enter 0 for Buyer and 1 for Seller");
+        int userType = sc.nextInt();
+
+        System.out.println("Enter your username: ");
+        String username = sc.next();
+
+        System.out.println("Enter your password: ");
+        String password = sc.next();
+
         Facade facade = new Facade();
-        facade.login();
+        facade.login(userType, username, password);
     }
 }
