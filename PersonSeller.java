030e13f2e3303b862a0c6c9c9d83025f3a3f486d
Aaditya Muley <aadityamuley8190@gmail.com>
2022-10-19 19:38:12 -0700

    Bridge pattern implemented

---
+++ PersonSeller.java
@@ -0,0 +1,48 @@
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class PersonSeller extends Person {
+    Scanner sc = new Scanner(System.in);
+
+    public void showMenu() throws FileNotFoundException {
+        
+        System.out.println("1. View Meat menu");
+        System.out.println("2. View Produce menu");
+        int choice = sc.nextInt();
+
+        switch(choice) {
+            case 1:
+            product = new ProductMeat();
+            product.showMenu();
+            break;
+
+            case 2:
+            product = new ProductProduce();
+            product.showMenu();
+        }
+    }
+
+    public void sellerMain() throws FileNotFoundException {
+
+        boolean loop = true;
+        while(loop == true) {
+            System.out.println("What would you like to do:");
+            System.out.println("1. View menu");
+            System.out.println("99. Exit");
+            int choice = sc.nextInt();
+
+            switch(choice) {
+                case 1:
+                System.out.println();
+                showMenu();
+                break;
+
+                case 99:
+                System.out.println("Bye!");
+                loop = false;
+                break;
+            }
+        }
+        
+    }
+}
