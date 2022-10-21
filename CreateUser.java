153cf2da8c1b8436928153c0e4801cce4d3e23c2
Aaditya Muley <aadityamuley8190@gmail.com>
2022-10-20 22:01:41 -0700

    Functionality to add users added. Visitor pattern complete

---
+++ CreateUser.java
@@ -0,0 +1,50 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.util.Scanner;
+
+public class CreateUser {
+
+    Scanner sc = new Scanner(System.in);
+    
+    public void createBuyer() {
+
+        System.out.println("Enter your username: ");
+        String username = sc.next();
+        System.out.println("Enter your password: ");
+        String password = sc.next();
+
+        String credential = "\n" + username + ":" + password;
+
+        try {
+            File buyerInfo = new File("data/BuyerInfo.txt");
+            FileWriter fw = new FileWriter(buyerInfo, true);
+            fw.append(credential);
+            fw.flush();
+            fw.close();
+        } 
+        catch (Exception e) {
+            System.err.println(e);
+        }
+    }
+
+    public void createSeller() {
+
+        System.out.println("Enter your username: ");
+        String username = sc.next();
+        System.out.println("Enter your password: ");
+        String password = sc.next();
+
+        String credential = "\n" + username + ":" + password;
+
+        try {
+            File buyerInfo = new File("data/SellerInfo.txt");
+            FileWriter fw = new FileWriter(buyerInfo, true);
+            fw.append(credential);
+            fw.flush();
+            fw.close();
+        } 
+        catch (Exception e) {
+            System.err.println(e);
+        }
+    }
+}
